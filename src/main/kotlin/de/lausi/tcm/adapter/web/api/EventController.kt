c1ab913f0bc2abf3a310e6f733ec902992bd800c
Tom Lausmann <tom.lausmann@unitb.de>
2024-05-03 16:41:38 +0200

    feat: add api/events request mapping

--- src/main/kotlin/de/lausi/tcm/adapter/web/api/EventController.kt
+++ src/main/kotlin/de/lausi/tcm/adapter/web/api/EventController.kt
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.UUID
@@ -35,6 +36,7 @@ data class CreateEventRequest(
 )
 
 @Controller
+@RequestMapping("/api/events")
 class EventController(
   private val eventRepository: EventRepository,
   private val courtRepository: CourtRepository,
